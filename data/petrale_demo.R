c6cc520009345748beafb11bc9a048964f68f840
Schiano-NOAA <samantha.schiano@noaa.gov>
2024-12-30 13:14:21 -0500

    Update demo script with current v for testing and other commented out optional pieces

--- data/petrale_demo.R
+++ data/petrale_demo.R
@@ -8,11 +8,28 @@ install.packages('remotes')
 install.packages('tinytex')
 library(tinytex)
 
-remotes::install_github("nmfs-ost/asar") # automated stock assessment reporting
+remotes::install_github("nmfs-ost/asar@dev-1.0") # automated stock assessment reporting
 remotes::install_github("nmfs-ost/satf") # stock assessment tables and figures
 
 # Load here to form relative paths for files
 library(here)
+
+# Optional: convert output first
+# asar::convert_output(
+#   output_file = file.path(getwd(), "data", "Report.sso"),
+#   model = "ss3",
+#   file_save = TRUE,
+#   savedir = file.path(getwd(), "data"),
+#   save_name = "Petrale_sole_std_res"
+# )
+
+# Optional: create all figures and tables before creating template
+#           *must run convert_output first
+# satf::exp_all_figs_tables(
+#   output,
+#   ref_line = "msy",
+#   ref_line_sb = "msy"
+# )
 
 # Template
 asar::create_template(
@@ -28,37 +45,50 @@ asar::create_template(
   simple_affiliation = FALSE,
   param_names = c("nf","sf"),
   param_values = c("North fleet", "South fleet"),
-  convert_output = TRUE,
-  resdir = here::here(),
-  model_results = "Report.sso",
-  model = "SS3"
+  # convert_output = TRUE, # comment out this line when output is already converted to standard framework
+  # resdir = here::here("data"), # comment out this line when output is already converted to standard framework
+  # model_results = "Report.sso", # comment out this line when output is already converted to standard framework
+  # model = "SS3", # comment out this line when output is already converted to standard framework
+  resdir = file.path(getwd(), "report"), # comment out this line when there is no standard output
+  model_results = "Petrale_sole_std_res_2023.csv", # comment out this line when there is no standard output
+  rda_dir = here::here('report'),
+  ref_line = "msy",
+  ref_line_sb = "msy"
 )
 
-# Read in output file
-  # output <- read.csv(
-  #   here::here("stock_assessment_reports",
-  #              "NWFSC",
-  #              "Petrale sole",
-  #              "U.S. West Coast",
-  #              "2023",
-  #              "Petrale_sole_std_res_2023.csv")
-  #   )
+#### Debugging ####
+
+# Uncomment to read in output from converted in create_template and execute fxns below
+# output <- utils::read.csv(here::here("report","Petrale_sole_std_res_2023.csv"))
 
 # See set up of standard output file
-  # View(output)
+# View(output)
 
-# Read in and view SS3 Report.sso file
-  # get_ncol <- function(file, skip = 0) {
-  #   nummax <- max(utils::count.fields(file,
-  #                                     skip = skip, quote = "",
-  #                                     comment.char = ""
-  #   )) + 1
-  #   return(nummax)
-  # }
-  # SS3_output <- read.table(
-  #   file = here::here("Report.sso"),
-  #   col.names = 1:get_ncol(here::here("Report.sso")), fill = TRUE, quote = "",
-  #   colClasses = "character", nrows = -1, comment.char = "",
-  #   blank.lines.skip = FALSE
-  # )
-  # View(SS3_output)
+# Uncomment if rdas were not created from create_template
+# satf::exp_all_figs_tables(
+#   output,
+#   ref_line = "msy",
+#   ref_line_sb = "msy"
+# )
+
+# Uncomment if biomass plot was not created
+# satf::plot_biomass(
+#   output,
+#   ref_line = "msy", # change reference to fit data
+#   make_rda = TRUE
+# )
+
+# Uncomment if spawning biomass plot was not created
+# satf::plot_spawning_biomass(
+#   output,
+#   ref_line = "msy", # change reference to fit data
+#   make_rda = TRUE
+# )
+
+# Uncomment if spawn recruitment plot was not created
+# satf::plot_spawn_recruitment(
+#   output,
+#   end_year = 2022,
+#   make_rda = TRUE
+# )
+
